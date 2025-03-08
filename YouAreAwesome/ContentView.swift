c788418f3cb0634c042a5ff7a6f9ff6686692dca
Bluetenhonig <linda.samsinger@uzh.ch>
2025-03-08 09:44:20 +0100

    add randomization and [repeat] while loop

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,58 +11,83 @@ struct ContentView: View {
     
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
+    @State private var lastImageNumber = -1 // if you put 0, 0 will never be shown at the first time, but we want to potentially show te first time
+    @State private var lastMessageNumber = -1
     
     var body: some View {
         
         
         VStack {
             
-            Spacer()
+            
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.50) // shrink text that is too long, so it is still readable
+                .frame(height: 130) // fixed height for text view
+            
+                .animation(.easeInOut(duration: 0.15), value: message) // best animation
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-             
+                .animation(.default, value: imageName)
             
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(.red)
+            
             
             Spacer()
             
+            // with randomization, repeats are displayed, but we do not want repeats
             
             Button("Press Me") {
-                let message1 = "You Are Awesome!"
-                let message2 = "You Are Great!"
-                
+                let messages = ["You Are Awesome",
+                                "Gadzook my friend! I am astonished at how utterly magnificent you are!", "When the Genius Bar Needs Help, They Call You!", "You Are Great", "Fabulous? That's You!", "Hey You!", "You Are Amazing"]
                 // ternary operator
-                message = (message == message1 ? message2 : message1)
-                imageName = (imageName == "image0" ? "image1" : "image0")
+                //                message = (message == message1 ? message2 : message1)
+                //                imageName = (imageName == "image0" ? "image1" : "image0")
                 
                 //TODO: - Update the imageName variable -
-                // todo, fixme, mark, -dashes add lines-
-                
+                // todo, fixme, mark all caps with colon and space, even more prominently -dashes add lines-
+                // breadcrumb trail is the file structure with a jump bar
+                var imageNumber: Int
+                repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
                 imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
                 
-                imageNumber += 1
+                var messageNumber: Int
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                } while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
                 
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
+                //                messageNumber += 1
+                //                imageNumber += 1
+                //
+                //                if messageNumber == messages.count {
+                //                    messageNumber = 0
+                //                }
+                
+                //                if imageNumber > 9 {
+                //                    imageNumber = 0
+                //                }
                 
                 print(imageNumber)
                 // if-else operator
-//                if message == message1 {
-//                    message = message2
-//                    image = "sun.max.fill"
-//                } else {
-//                    message = message1
-//                    image = "hand.thumbsup"
-//                }
+                //                if message == message1 {
+                //                    message = message2
+                //                    image = "sun.max.fill"
+                //                } else {
+                //                    message = message1
+                //                    image = "hand.thumbsup"
+                //                }
                 
                 
             }
